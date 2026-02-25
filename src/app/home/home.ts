e67ec1b7ba81f2b8ad855aac4e72b169830650b3
pierre.ingrachen <pierre.ingrachen--boutantin@epfedu.fr>
2026-02-25 14:38:04 +0100

    Ajout du carousel

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
 import { MoviesApi } from '../services/movies-api';
-import { inject } from '@angular/core';
 import { Movie } from '../models/movies';
 import { Observable } from 'rxjs';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { MovieCard } from './movie-card/movie-card';
+import { register } from 'swiper/element/bundle';
 
+register();
 @Component({
   selector: 'app-home',
   imports: [AsyncPipe, DatePipe, MovieCard],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Home {
   private readonly moviesApi = inject(MoviesApi)
   movies$: Observable<Movie[]> = this.moviesApi.getMovies()
+  
 }
