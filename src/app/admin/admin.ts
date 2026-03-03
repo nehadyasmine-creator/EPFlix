e4dcfec5e826d12a1a897251f7bbf0987902091f
martin.ladan <martin.ladan@epfedu.fr>
2026-03-03 12:08:21 +0100

    Ajout liste utilisateurs coté admin + suppression

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, inject , DestroyRef} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ToastService } from '../services/toast';
-import { inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+
+import { AuthService } from '../services/auth-service';
+import { User } from '../models/users';
+
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 
 
 @Component({
@@ -22,9 +28,14 @@ export class Admin implements OnInit {
 
   private toastService = inject(ToastService);
   private router = inject(Router);
+  private destroyRef = inject(DestroyRef);
 
   isLoggedInAdmin: boolean = false;
 
+  private authService = inject(AuthService);
+
+  users = signal<User[]>([]);
+  isLoadingUsers = signal<boolean>(false);
 
   ngOnInit() {
     const savedStatus = sessionStorage.getItem('admin_access');
@@ -37,10 +48,10 @@ export class Admin implements OnInit {
     this.loginError = null;
     if (this.saisieCode === this.secretCode) {
       this.toastService.show('Connexion Administrateur réussie !', { classname: 'bg-info text-white' });
-    //  setTimeout(
-       // this.loading = true,
-        //1500
-    //  )
+      //  setTimeout(
+      // this.loading = true,
+      //1500
+      //  )
       this.loading = true;
       sessionStorage.setItem('admin_access', 'true');
       this.isLoggedInAdmin = true;
@@ -55,20 +66,40 @@ export class Admin implements OnInit {
   }
 
   adminLogOut() {
-  sessionStorage.removeItem('admin_access');
-  this.isLoggedInAdmin = false; 
+    sessionStorage.removeItem('admin_access');
+    this.isLoggedInAdmin = false;
+
+    this.router.navigate(['/admin']);
+  }
+
+  ligneChoisie: string = '';
+
+  setContenu(key: string) {
+    this.ligneChoisie = key;
+  }
+
   
-  this.router.navigate(['/admin']); 
+
+  loadAllUsers() {
+    this.isLoadingUsers.set(true);
+    this.authService.getAllUsers()
+      .pipe(finalize(() => this.isLoadingUsers.set(false)))
+      .subscribe({
+        next: (data) => this.users.set(data),
+        error: () => this.toastService.show("Erreur de chargement", { classname: 'bg-danger text-white' })
+      });
+  }
+  deleteUser(id: number): void {
+    this.authService.deleteUser(id).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => 
+      this.users.update(current => current.filter(user => user.id !== id))
+    );
+  }
+
+clickMethod(id: number, firstname:string, lastname:string) {
+  if(confirm("Etes-vous sûr de vouloir supprimer "+firstname+" "+lastname+" ?")) {
+    this.deleteUser(id);
+  }
 }
-
-  ligneChoisie : string ='';
-
-  setContenu(key:string){
-      this.ligneChoisie = key;
-    }
-  
-
-  
 
 
 }
