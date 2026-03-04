e6403fb999c14a4f36166c22f7fa844497ed143c
pierre.ingrachen <pierre.ingrachen--boutantin@epfedu.fr>
2026-03-04 09:18:52 +0100

    Taille des movie-card

--- src/app/home/movie-card/movie-card.ts
+++ src/app/home/movie-card/movie-card.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
 import { Movie } from '../../models/movies';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss',
 })
 export class MovieCard {
-  @Input({required: true}) movie!: Movie
+  @Input({required: true}) movie!: Movie;
+  isExpanded = false;
 
   getMovieUrl(title: string): string {
     return title.toLowerCase().replace(/\s+/g, '-');
+  }
+
+  toggleExpand(event: Event) {
+    event.stopPropagation(); // Empêche de déclencher le [routerLink] de la card
+    this.isExpanded = !this.isExpanded;
   }
 }
